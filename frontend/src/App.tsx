7786ef9a48b0fc9415903e25b52acae6ad470049
Luiz Batista <10695871+oggiie@users.noreply.github.com>
2022-09-10 22:54:36 -0300

    Toast
    Implementação de mensagem de confirmação de envio de SMS ao usuário

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import NotificationButton from "./components/NotificationButton";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 
 import SalesCard from "./components/SalesCard";
@@ -7,6 +8,7 @@ import SalesCard from "./components/SalesCard";
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
